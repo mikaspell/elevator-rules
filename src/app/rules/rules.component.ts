185907cac648d49f82be9ee4ce9c04d6fae19637
mikaspell <work@melkee.ru>
2018-08-21 17:43:44 +0300

    Elevator's rules component

--- src/app/rules/rules.component.ts
+++ src/app/rules/rules.component.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
 
+export interface Rule {
+  name: string,
+  title: string,
+  isActive?: boolean
+}
+
 @Component({
   selector: 'app-rules',
   templateUrl: './rules.component.html',
   styleUrls: ['./rules.component.scss']
 })
 
-export class RulesComponent  {}
+export class RulesComponent  {
+  result: object = {
+    weight: true,
+    distance: true
+  };
+
+  rules:Rule[] = [
+    {
+      name: 'weight',
+      title: 'Учитывать вес пассажиров'
+    },
+    {
+      name: 'direction',
+      title: 'Учитывать направление лифта'
+    },
+    {
+      name: 'distance',
+      title: 'Учитывать расстояние до необходимого этажа'
+    }
+  ]
+
+}
