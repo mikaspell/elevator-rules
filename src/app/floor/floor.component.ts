bf6c0a3965c03185fa0d3877601420959ebb4c9f
mikaspell <work@melkee.ru>
2018-11-12 17:27:21 +0300

    fix

--- src/app/floor/floor.component.ts
+++ src/app/floor/floor.component.ts
@@ -1,21 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
-export interface Floor {
-  lvl: number,
-  status: string,
-  direction: string
-}
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-floor',
   templateUrl: './floor.component.html',
   styleUrls: ['./floor.component.scss']
 })
-export class FloorComponent implements OnInit {
+export class FloorComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
+  
+  @Input() number: number;
 
 }
