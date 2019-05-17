307356cd5fc0ad40f658dc6663f4e0fddfe5103a
mikaspell <work@melkee.ru>
2019-05-17 13:36:16 +0300

    reworked elevators service

--- src/app/elevators.service.ts
+++ src/app/elevators.service.ts
@@ -1,25 +1,57 @@
-import {Injectable} from "@angular/core";
-import {Elevator} from "./types";
+import {Injectable} from '@angular/core';
+import {Elevator} from './types';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable()
-
 export class ElevatorsService {
-  
-  private elevators: Elevator[] = [];
-  
-  getElevators(): Elevator[] {
-    return this.elevators;
+
+  private _elevators: BehaviorSubject<Elevator[]> = new BehaviorSubject<Elevator[]>([]);
+
+  constructor() {
+    this.addElevator(1);
   }
-  
-  addElevator(): void {
-    this.elevators.push({
-      id: this.elevators.length + 1,
-      target: null,
-      weight: 0,
-      maxWeight: 680,
-      status: 'waiting',
-      direction: null,
-      position: 1
-    })
+
+  get elevators() { return this._elevators.asObservable(); }
+  get elevatorsCount() { return this._elevators.getValue().length; }
+  get logs() { return console.log(this._elevators.getValue()); }
+
+  addElevator(count: number): void {
+    let elevatorsArray: Elevator[] = this._elevators.getValue();
+    let elevatorsArrayLength: number = this.elevatorsCount;
+    let i = 0;
+
+    while (i < count) {
+      elevatorsArray.push({
+        id: ++elevatorsArrayLength,
+        target: null,
+        weight: 0,
+        maxWeight: 680,
+        status: 'waiting',
+        direction: null,
+        position: 1
+      });
+      i++;
+    }
+
+    this._elevators.next(elevatorsArray);
+    this.logs;
+  }
+
+  deleteElevator(count: number): void {
+    let elevatorsArray: Elevator[] = this._elevators.getValue();
+    let result = elevatorsArray.slice(0, elevatorsArray.length - count);
+
+    this._elevators.next(result);
+    this.logs;
+  }
+
+  methodsRouter(value) {
+    let count = this.elevatorsCount;
+
+    if (count > value) {
+      this.deleteElevator(count - value);
+    } else {
+      this.addElevator(value - count);
+    }
   }
 }
