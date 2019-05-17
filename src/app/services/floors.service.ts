ce8e1232d8ca8a3d62f88985604e5bb39e08b97b
mikaspell <work@melkee.ru>
2019-05-17 16:22:54 +0300

    reworked floors service

--- src/app/services/floors.service.ts
+++ src/app/services/floors.service.ts
@@ -1,28 +1,51 @@
-import {Injectable} from "@angular/core";
-import {Floor} from "../types";
+import {Injectable} from '@angular/core';
+import {Floor} from '../types';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable()
-
 export class FloorsService {
-  
-  private floors: Floor[];
-  
-  getFloors(): Floor[] {
-    return this.floors;
+
+  private _floors: BehaviorSubject<Floor[]> = new BehaviorSubject<Floor[]>([]);
+
+  constructor() {
+    this.add(5);
   }
-  
-  generatefloors(count:number): void {
-    this.floors = new Array(count);
-    
-    for(let i=0; i<this.floors.length; i++) {
-      this.floors[i] = {
-        title: count - i
-      };
+
+  get floors(): Observable<Floor[]> { return this._floors.asObservable(); }
+  get floorsCount(): number { return this._floors.getValue().length; }
+  get logs(): void { return console.log(this._floors.getValue()); }
+
+  add(count: number) {
+    let floors: Floor[] = this._floors.getValue();
+    let floorCount: number = this.floorsCount;
+    let i = 0;
+
+    while (i < count) {
+      floors.push({
+        title: ++floorCount
+      });
+      i++;
     }
+
+    this._floors.next(floors);
+    this.logs;
   }
-  
-  addFloor():void {
-    
-    this.floors.push({title: this.floors.length + 1});
+
+  delete(count: number) {
+    let floors: Floor[] = this._floors.getValue();
+    let result = floors.slice(0, floors.length - count);
+
+    this._floors.next(result);
+    this.logs;
+  }
+
+  methodsRouter(value: number) {
+    let count = this.floorsCount;
+
+    if (count > value) {
+      this.delete(count - value);
+    } else {
+      this.add(value - count);
+    }
   }
 }
